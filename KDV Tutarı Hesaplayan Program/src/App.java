13bba75e0514c1a2fc37f97a70bc05ffa9cd8566
unknown <mstfsahin14@gmail.com>
2023-10-08 21:37:45 +0300

    KDV Tutarı Hesaplayan Program eklendi

--- KDV Tutarı Hesaplayan Program/src/App.java
+++ KDV Tutarı Hesaplayan Program/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println("KDV'siz fiyatı girin");
+        Scanner asd = new Scanner(System.in);
+        double girdi = asd.nextDouble();
+        if (girdi<1000)
+            System.out.println("KDV'siz fiyat = "+girdi + "\nKDV'li fiyat = "+girdi*(1.18) + "\nKDV Tutarı = "+girdi*(0.18) );
+        else
+            System.out.println("KDV'siz fiyat = "+girdi + "\nKDV'li fiyat = "+girdi*(1.08) + "\nKDV Tutarı = "+girdi*(0.08) );
     }
 }
